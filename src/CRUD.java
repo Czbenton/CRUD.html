454b068a067728c83b279141cddfb41300334ed2
Zach Benton <Zach@Zach.local>
2016-09-27 08:41:53 -0500

    init

---
+++ src/CRUD.java
@@ -0,0 +1,178 @@
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by Zach on 9/26/16.
+ */
+public class CRUD {
+    static ArrayList<SuperHero> heroList = new ArrayList<>();
+    static HashMap<String, User> users = new HashMap<>();
+
+    public static void main(String[] args) {
+        Spark.init();
+
+        addTestInformation();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+
+                    Session session = request.session();
+                    String userName = session.attribute("userName");
+
+                    for (SuperHero s : heroList ){
+                        if(s.userName.equalsIgnoreCase(userName)) {
+                            s.owner = "notNull";
+                        }else{
+                            s.owner = null;
+                        }
+                    }
+
+                    m.put("userName", userName);
+                    m.put("hero", heroList);
+                    return new ModelAndView(m,"home.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                ((request, response) -> {
+                    String username = request.queryParams("loginName");
+                    String password = request.queryParams("password");
+                    if (username == null) {
+                        throw new Exception("login name not found");
+                    }
+                    User user = users.get(username);
+                    if (user == null) {
+                        user = new User(username,password);
+                        users.put(username, user);
+                    }
+                    Session session = request.session();
+                    session.attribute("userName", username);
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/newEntry",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String userName = session.attribute("userName");
+                    if (userName == null) {
+                        throw new Exception("not logged in");
+                    }
+
+
+
+                    String secretIdentity = request.queryParams("secretIdentity");
+                    String heroName = request.queryParams("heroName");
+                    int age = Integer.parseInt(request.queryParams("age"));
+                    String powersString = request.queryParams("powersString");
+                    ArrayList<String> powersList = new ArrayList<String>(Arrays.asList( powersString.split(",")));
+                    String primaryCostumeColor = request.queryParams("primaryColor");
+                    String secondaryCostumeColor = request.queryParams("secondaryColor");
+
+                    SuperHero superHero = new SuperHero(userName, secretIdentity,heroName,age,powersList,primaryCostumeColor,secondaryCostumeColor);
+
+                    heroList.add(superHero);
+
+
+                    response.redirect(request.headers("Referer"));
+                    return "";
+                })
+        );
+        Spark.post(
+                "/deleteHero",
+                ((request, response) -> {
+                    String heroName = request.queryParams("heroName");
+                    SuperHero toRemove = new SuperHero();
+                    for (SuperHero s: heroList) {
+                        if(s.heroName.equalsIgnoreCase(heroName)){
+                            toRemove = s;
+                        }
+                    }
+                    heroList.remove(toRemove);
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/editHero",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String userName = session.attribute("userName");
+                    String heroName = session.attribute("heroName");
+                    SuperHero toEdit = new SuperHero();
+                    for (SuperHero s: heroList) {
+                        if(s.heroName.equalsIgnoreCase(heroName)){
+                            toEdit = s;
+                        }
+                    }
+                    heroList.remove(toEdit);
+
+                    String secretIdentity = request.queryParams("secretIdentity");
+                    String newHeroName = request.queryParams("heroName");
+                    int age = Integer.parseInt(request.queryParams("age"));
+                    String powersString = request.queryParams("powersString");
+                    ArrayList<String> powersList = new ArrayList<String>(Arrays.asList( powersString.split(",")));
+                    String primaryCostumeColor = request.queryParams("primaryColor");
+                    String secondaryCostumeColor = request.queryParams("secondaryColor");
+
+                    SuperHero superHero = new SuperHero(userName, secretIdentity,newHeroName,age,powersList,primaryCostumeColor,secondaryCostumeColor);
+
+
+                    heroList.add(superHero);
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.get(
+                "/editPage",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    Session session = request.session();
+                    String userName = session.attribute("userName");
+
+                    for (SuperHero s : heroList ){
+                        if(s.userName.equalsIgnoreCase(userName)) {
+                            s.owner = "notNull";
+                        }else{
+                            s.owner = null;
+                        }
+                    }
+
+                    m.put("hero",heroList);
+                    return new ModelAndView(m,"editPage.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+
+    }
+
+    public static void addTestInformation(){
+        ArrayList<String> powers = new ArrayList<>(Arrays.asList("billionaire","martial arts","genius","world's greatest detective"));
+
+        SuperHero superHero = new SuperHero("zach","Bruce Wayne", "Batman", 30, powers,"black","grey");
+
+        heroList.add(superHero);
+    }
+}
---
+++ src/SuperHero.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Zach on 9/26/16.
+ */
+public class SuperHero {
+    String secretIdentity;
+    String heroName;
+    int age;
+    ArrayList<String> knownHeroPowers = new ArrayList<>();
+    String primaryCostumeColor;
+    String secondaryCostumeColor;
+    String userName;
+    String owner;
+
+    public SuperHero(){}
+
+    public SuperHero(String ownerName, String secretIdentity, String heroName, int age, ArrayList<String> knownHeroPowers,
+                     String primaryCostumeColor, String secondaryCostumeColor) {
+        this.userName = ownerName;
+        this.secretIdentity = secretIdentity;
+        this.heroName = heroName;
+        this.age = age;
+        this.knownHeroPowers = knownHeroPowers;
+        this.primaryCostumeColor = primaryCostumeColor;
+        this.secondaryCostumeColor = secondaryCostumeColor;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s, "+age+", "+knownHeroPowers+", %s, %s", secretIdentity,heroName,
+                primaryCostumeColor,secondaryCostumeColor);
+    }
+}
